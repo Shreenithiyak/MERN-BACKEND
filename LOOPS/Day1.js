7afc0486f6ca9e49dc69201d29b6a22e5d522841
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-02-24 15:06:14 +0530

    update

---
+++ LOOPS/Day1.js
@@ -0,0 +1,26 @@
+// What is a loop?
+//  Loops can execute a block of code a number of times.
+
+// for loop syntax
+   for (let i = 0; i < 5; i++) 
+// while loop
+  while(condition){
+    i++
+  }
+// do...while
+do {
+    i++
+}
+while(conditions)
+// Difference between them
+// for Loop	                                       while Loop	                                     do...while Loop
+
+
+
+⭐ Simple One-Line Meaning
+
+for → "I know how many times."
+
+while → "Run until condition becomes false."
+
+do...while → "Run first, then check condition."
