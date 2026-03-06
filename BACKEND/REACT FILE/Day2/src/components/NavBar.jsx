e929275c2829dba44b8f5c284e4a26bb31f3e04e
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-03-06 10:04:58 +0530

    zdfghjkl;

---
+++ BACKEND/REACT FILE/Day2/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import component from "react"
+
+class NavBar extends component{
+
+ render(){
+    return(
+    <>
+      <div className="navbar">
+      <a href="">Home</a>
+       <a href="">about</a>
+        <a href="">contact</a>
+        <a href="">login</a>
+
+      </div>
+    
+    
+    
+    
+    
+    </>
+    )
+ }
+}
+
+export default NavBar
