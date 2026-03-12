6decf84f7fdd45be7c1e885b36fcc9ad6b5d90ad
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-03-12 23:59:16 +0530

    changed

---
+++ BACKEND/REACT FILE/Day7,8/navlink/src/Approutes.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom"
+import Login from "./components/Login"
+import Dashboardlayout from "./components/Layout"
+import Dashboard from "./components/Dashboard"
+import Profile from "./components/Profile"
+
+
+const Approutes = () => {
+  return (
+    <>
+    <Routes>
+    <Route element={< Dashboardlayout/>}>
+  
+    <Route path="/ Dashboard" element={< Dashboard/>}/>
+    <Route path="/ Profile" element={<Profile/>}/>
+    </Route>
+
+    <Route path="/" element={<Login/>} />
+    
+    </Routes>
+    </>
+  )
+}
+
+export default Approutes
+
+
+
+
+
+
---
+++ BACKEND/REACT FILE/Day7,8/navlink/src/components/Login.jsx
@@ -0,0 +1,12 @@
+
+function About(){
+  return(
+    <>
+    <input type="text" placeholder="Enter Your Name" />
+    <button>Submit</button>
+    
+
+    </>
+  )
+}
+export default About
