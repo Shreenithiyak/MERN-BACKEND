d17f4b35de859afb84741d17dd767c8101967e63
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-03-24 11:32:46 +0530

    fixed responsive styles

--- BACKEND/REACT-FILE/Day6/reactrouters/src/App.jsx
+++ BACKEND/REACT-FILE/Day6/reactrouters/src/App.jsx
@@ -3,21 +3,25 @@ import AppRoute from './Routes/AppRoute'
 
 const App = () => {
   return (
-   <>
-   <div className="text-lg sm:text-xl md:text-3xl">
-    <nav className="flex  bg-blue-500 p-4">
-    <div className="flex  ">
-      <h1 className="flex text-3xl ">ASSIGNMENTS</h1>
-    </div>
-    <div className="flex ml-300 gap-9 text-2xl hover:text-green-500">
-     <Link to="/" className="text-white">Introduction</Link>
-      <Link to="/createphase" className="text-white">Create Phase</Link>
-      <Link to="/updatephase" className="text-white">Update Phase</Link>
-    </div>
-    </nav>
-      <AppRoute/>
+    <>
+      <div>
+        <nav className="flex bg-blue-500 p-4 items-center">
+          
+          <h1 className="text-lg sm:text-xl md:text-3xl text-white">
+            ASSIGNMENTS
+          </h1>
+
+          <div className="flex ml-auto gap-4 sm:gap-6 md:gap-9 text-sm sm:text-lg md:text-2xl">
+            <Link to="/" className="text-white hover:text-green-500">Introduction</Link>
+            <Link to="/createphase" className="text-white hover:text-green-500">Create Phase</Link>
+            <Link to="/updatephase" className="text-white hover:text-green-500">Update Phase</Link>
+          </div>
+
+        </nav>
+
+        <AppRoute />
       </div>
-   </>
+    </>
   )
 }
 
