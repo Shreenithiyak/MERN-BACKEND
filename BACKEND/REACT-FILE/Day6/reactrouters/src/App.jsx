01ba441b44829d070eefbbe79e5b7384dc46925f
nithiyashreek2004-design <nithiyashreek2004@gmail.com>
2026-03-24 11:16:58 +0530

    fixed responsive styles

--- BACKEND/REACT-FILE/Day6/reactrouters/src/App.jsx
+++ BACKEND/REACT-FILE/Day6/reactrouters/src/App.jsx
@@ -4,6 +4,7 @@ import AppRoute from './Routes/AppRoute'
 const App = () => {
   return (
    <>
+   <div className="text-3xl md:text-xl sm:text-lg">
     <nav className="flex  bg-blue-500 p-4">
     <div className="flex  ">
       <h1 className="flex text-3xl ">ASSIGNMENTS</h1>
@@ -12,9 +13,10 @@ const App = () => {
      <Link to="/" className="text-white">Introduction</Link>
       <Link to="/createphase" className="text-white">Create Phase</Link>
       <Link to="/updatephase" className="text-white">Update Phase</Link>
-</div>
+    </div>
     </nav>
       <AppRoute/>
+      </div>
    </>
   )
 }
